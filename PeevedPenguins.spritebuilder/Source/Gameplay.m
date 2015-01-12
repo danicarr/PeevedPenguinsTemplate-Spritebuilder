e95b73922c64cf1b85c85af115745dd5202bec21
danicarr <danicarr@mit.edu>
2015-01-12 01:07:45 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -12,7 +12,7 @@
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
     CCNode *_levelNode;
-    CCNode *_contentCode;
+    CCNode *_contentNode;
 }
 
 // is called when CCB file has completed loading
