436f51ae41e47837d76542773c789f9c3377a4ee
danicarr <danicarr@mit.edu>
2015-01-11 20:55:12 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,23 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by danicarr on 1/10/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+-(id) init{
+    self= [super init];
+    
+    if(self){
+        CCLOG(@"Penguin Created!");
+    }
+    
+    return self;
+}
+
+@end
